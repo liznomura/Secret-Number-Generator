9c5d057855cab5356ab4be764d6b1beab8fdd599
liznomura <lizn@hawaii.edu>
2017-06-15 12:18:38 -1000

    2 passing, 1 failing - Secret Number does not return same # every time it is invoked

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,9 @@
 'use strict';
+
 module.exports = function() {
-  
+  var secretNumber = 0;
+  return function() {
+    secretNumber = Math.random() * (1000000 - 0);
+    return secretNumber;
+  };
 };
